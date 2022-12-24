48a16dd4ebb64edb147b4515281de48e71b15122
Allen Li <113055658+Allen-Li-hub@users.noreply.github.com>
2022-12-24 15:18:34 -0500

    change

--- main.cs
+++ main.cs
@@ -1,7 +1,7 @@
 // Created by: Allen Li
-// Created on: Oct 2022
+// Created on: Dec 2022
 //
-// This program displays, "Hello, World!"
+// This program displays, "Triangle type checker"
 
 using System;
 
@@ -9,7 +9,54 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // This function accepts user input
+        int lengthA;
+        int lengthB;
+        int lengthC;
+        double angleA;
+        double angleB;
+        double angleC;
+        double sumOfAngles;
+
+        Console.WriteLine("Let's check the type of the triangle!");
+        Console.WriteLine("");
+
+        Console.WriteLine("Enter the following lengths: ");
+        Console.WriteLine("");
+        Console.WriteLine("Length A: ");
+        lengthA = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Length B: ");
+        lengthB = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Length C: ");
+        lengthC = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine(" ");
+        angleA = Math.Acos((Math.Pow(lengthB, 2) + Math.Pow(lengthC, 2) - Math.Pow(lengthA, 2)) / (2 * lengthB * lengthC)) * (180 / Math.PI);
+        angleB = Math.Acos((Math.Pow(lengthC, 2) + Math.Pow(lengthA, 2) - Math.Pow(lengthB, 2)) / (2 * lengthC * lengthA)) * (180 / Math.PI);
+        angleC = Math.Acos((Math.Pow(lengthA, 2) + Math.Pow(lengthB, 2) - Math.Pow(lengthC, 2)) / (2 * lengthA * lengthB)) * (180 / Math.PI);
+
+        sumOfAngles = angleA + angleB + angleC;
+        Console.WriteLine("Sum of all angles: " + sumOfAngles.ToString("0.0"));
+        Console.WriteLine(" ");
+
+
+        if (angleA == angleB && angleA == angleC)
+        {
+            Console.WriteLine("You have an equalatiral triangle!");
+        }
+        else if (angleA != angleB && angleA != angleC && angleB != angleC)
+        {
+            Console.WriteLine("You have an scalene triangle!");
+        }
+        else if ((angleA != angleB && angleA == angleC) || (angleA == angleB && angleA != angleC))
+        {
+            Console.WriteLine("You have an isosceles triangle!");
+        }
+        else
+        {
+            Console.WriteLine("Umm. not a triangle");
+        }
+
         Console.WriteLine("\nDone.");
     }
 }
